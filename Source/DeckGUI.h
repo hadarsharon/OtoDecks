a0ba5971a694617860dbb23c4f18ae4f62e59abe
Hadar Sharon <hadars@crazylabs.com>
2022-03-05 15:53:49 +0200

    adding DeckGUI

---
+++ Source/DeckGUI.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    DeckGUI.h
+    Created: 5 Mar 2022 3:53:33pm
+    Author:  HadarS
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class DeckGUI  : public juce::Component
+{
+public:
+    DeckGUI();
+    ~DeckGUI() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DeckGUI)
+};
