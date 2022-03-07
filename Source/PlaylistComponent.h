5d58f22cb164f53a2417aa018ad77546a036a232
Hadar Sharon <hadars@crazylabs.com>
2022-03-07 22:23:54 +0200

    bout to add PlaylistComponent implementation

---
+++ Source/PlaylistComponent.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    PlaylistComponent.h
+    Created: 7 Mar 2022 10:23:33pm
+    Author:  HadarS
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class PlaylistComponent  : public juce::Component
+{
+public:
+    PlaylistComponent();
+    ~PlaylistComponent() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlaylistComponent)
+};
